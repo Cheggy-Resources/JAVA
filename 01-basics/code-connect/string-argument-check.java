c586c7ab996f127b02313db04357ebaecc354b91
Cheggy-Resources <cheggy824@gmail.com>
2026-04-26 15:01:51 +0530

    Create string-argument-check.java

---
+++ 01-basics/code-connect/string-argument-check.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+}
+// checking when we will pass the params for args string array. 
+// javac - does compilation work. 
+// java path.filename - run it
+// We will pass the args after filename (after leaving one space) and there will be single space between different inputs.
