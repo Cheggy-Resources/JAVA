47525b5f19fe96a9ce495314c1c6157c1ec0b2ae
Cheggy-Resources <cheggy824@gmail.com>
2026-04-26 15:32:45 +0530

    Update and rename first.java to First.java

--- 01-basics/code-connect/packages/First.java
+++ 01-basics/code-connect/packages/First.java
@@ -1,8 +1,9 @@
 // package JAVA.01-basics.code-connect.packages  
+
 // PACKAGE NAME IS NOT MENTIONED DUE TO 01-basics folder name. 
 // Can't start with numbers. BUT we need to provide numeric digits there FOR ordering purpose.
   
-public class first {
+public class First {
     int x = 5;
 }
 // Learning about packages.
