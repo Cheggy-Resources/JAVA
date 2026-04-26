13fd9775fa6a0220e96bc6850d4d409c547b6c89
Cheggy-Resources <cheggy824@gmail.com>
2026-04-26 15:32:26 +0530

    Update first.java

--- 01-basics/code-connect/packages/first.java
+++ 01-basics/code-connect/packages/first.java
@@ -1,4 +1,6 @@
-package JAVA.01-basics.code-connect.packages
+// package JAVA.01-basics.code-connect.packages  
+// PACKAGE NAME IS NOT MENTIONED DUE TO 01-basics folder name. 
+// Can't start with numbers. BUT we need to provide numeric digits there FOR ordering purpose.
   
 public class first {
     int x = 5;
