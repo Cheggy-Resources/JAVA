32217a2a43839df3331338b8c4b9a9125f587601
Cheggy-Resources <cheggy824@gmail.com>
2026-04-26 16:46:21 +0530

    Create abs_cls_interface_impl.java

---
+++ 02-oops/abstraction/code_connect/abs_cls_interface_impl.java
@@ -0,0 +1,10 @@
+public class abstract_cls_interface {
+   public abstract_cls_interface() {
+   }
+
+   public static void main(String[] var0) {
+      class3 var1 = new class3();
+      var1.display();
+      var1.helloNamste();
+   }
+}
