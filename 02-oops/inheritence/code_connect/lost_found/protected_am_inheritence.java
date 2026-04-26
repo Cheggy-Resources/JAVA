d59c275a4d8069c317dcec2dc98996ca4b3672ef
Cheggy-Resources <cheggy824@gmail.com>
2026-04-26 16:32:09 +0530

    Create protected_am_inheritence.java

---
+++ 02-oops/inheritence/code_connect/lost_found/protected_am_inheritence.java
@@ -0,0 +1,9 @@
+public class protected_am_inheritence {
+    int x;
+    public protected_am_inheritence(){
+        x = 5;
+    }
+    protected void display(){
+        System.out.println("The value of x is: " + x);
+    }
+}
