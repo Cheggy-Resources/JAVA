37bb4d7c52b237c7c84a5836fc112a517b0a0636
Cheggy-Resources <cheggy824@gmail.com>
2026-04-26 16:47:05 +0530

    Update abstract_class_implementing_interface.java

--- 02-oops/abstraction/code_connect/abstract_class_implementing_interface.java
+++ 02-oops/abstraction/code_connect/abstract_class_implementing_interface.java
@@ -1,8 +1,25 @@
-abstract class abstract_class2 implements abs_int {
-   abstract_class2() {
-   }
 
-   public void helloNamste() {
-      System.out.println("Hello, Namaste!");
-   }
+interface abs_int{
+    void display();
+    void helloNamste();
+}
+
+abstract class abstract_class2 implements abs_int{
+    public void helloNamste() {
+        System.out.println("Hello, Namaste!");
+    }
+}
+
+class class3 extends abstract_class2 {
+    public void display() {
+        System.out.println("This is the implementation of the display method in class3.");
+    }
+}
+
+public class abstract_cls_interface {
+    public static void main(String[] args) {
+        abs_int obj = new class3();
+        obj.display();
+        obj.helloNamste();
+    }
 }
